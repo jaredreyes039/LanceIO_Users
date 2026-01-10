7fcaf7c0d3ffb1a09f77a618538a3e9e297f1a44
Jay Reyes <jaredreyes039@gmail.com>
2026-01-10 00:03:47 -0700

    Critical change for security

--- config/database.config.js
+++ config/database.config.js
@@ -1,21 +1,21 @@
 var sql = require("mssql");
 
-exports.initSqlConnection  = (callback) => {
+exports.initSqlConnection = (callback) => {
 
-    var config = {
-        user: 'ADMIN',
-        password: '2dopamine2:3',
-        server: 'localhost', 
-        database: 'lanceio_users',
-        trustServerCertificate: true
-    };
+	var config = {
+		user: 'ADMIN',
+		password: '',
+		server: 'localhost',
+		database: 'lanceio_users',
+		trustServerCertificate: true
+	};
 
-    sql.connect(config, function (err) {
-                console.log("Initiating SQL Connection...")
-                if (err) console.log(err);
+	sql.connect(config, function(err) {
+		console.log("Initiating SQL Connection...")
+		if (err) console.log(err);
 
-                callback()                
+		callback()
 
-    });
+	});
 
 }
