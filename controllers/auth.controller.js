ab7ddc2c06133c731cf8548ed69c4ff0c9c49e3e
Jay Reyes <jaredreyes039@gmail.com>
2026-01-20 12:10:05 -0700

    Passport and localStrat implemented to eventually replace auth controller and move towards proxy architecture

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,5 @@
 import * as db from "../db/index.mjs"
 
-
-// TODO: INTERFACE WITH A LOGGER LIKE MORGAN
-// TODO: INTERFACE WITH PROMETHEUS FOR MONITORING
-// TODO: MOVE TO USER CONTROLLER
 async function checkIfUserExists(username, email) {
 	var users = await db.query('SELECT * FROM users WHERE username=$1 OR email=$2', [username, email])
 	if (users.length > 0) {
@@ -14,14 +10,13 @@ async function checkIfUserExists(username, email) {
 	}
 }
 
-// TODO: MOVE TO USER CONTROLLER
 async function encryptPassword(password) {
 	const salt = await bcrypt.genSalt(2);
 	const hashedPassword = await bcrypt.hash(password, salt); // Pass the salt  << - ~ - >>	
 	return hashedPassword;
 }
 
-async function verifyUser(username, password) {
+export async function verifyUser(username, password) {
 	try {
 		var query = await db.query('SELECT * FROM users WHERE username=$1', [username])
 		if (query.rows.length > 0) {
@@ -41,10 +36,4 @@ async function verifyUser(username, password) {
 	catch (err) {
 		return false;
 	}
-}
-
-export const loginHandler = async (req, res) => {
-	const { username, password } = req.body;
-	let verifiedUser = await verifyUser(username, password);
-	console.log(verifiedUser);
 }
